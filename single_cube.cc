4d9629b32ec50c746111b7d6b1de1b7d615b0f42
George <george.pking@gmail.com>
2022-11-17 19:03:17 +0000

    Update single_cube.cc

--- single_cube.cc
+++ single_cube.cc
@@ -6,7 +6,6 @@
         glEnable(GL_LIGHTING)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        # Load the modelview matrix from the current state of the trackball
         glMatrixMode(GL_MODELVIEW)
         glPushMatrix()
         glLoadIdentity()
@@ -19,16 +18,14 @@
         self.modelView = numpy.transpose(currentModelView)
         self.inverseModelView = inv(numpy.transpose(currentModelView))
 
-        # render the scene. This will call the render function for each object
-        # in the scene
         self.scene.render()
 
-        # draw the grid
+        # draw grid
         glDisable(GL_LIGHTING)
         glCallList(G_OBJ_PLANE)
         glPopMatrix()
 
-        # flush the buffers so that the scene can be drawn
+        # flush the buffers
         glFlush()
 
     def init_view(self):
@@ -36,7 +33,6 @@
         xSize, ySize = glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT)
         aspect_ratio = float(xSize) / float(ySize)
 
-        # load the projection matrix. Always the same
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
 
