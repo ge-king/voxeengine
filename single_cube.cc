39130541f7317d6908dbd0598dd5d56d9d1c640f
George <george.pking@gmail.com>
2022-11-17 19:02:37 +0000

    Create single_cube.cc

---
+++ single_cube.cc
@@ -0,0 +1,45 @@
+    # class Viewer
+    def render(self):
+        """ The render pass for the scene """
+        self.init_view()
+
+        glEnable(GL_LIGHTING)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # Load the modelview matrix from the current state of the trackball
+        glMatrixMode(GL_MODELVIEW)
+        glPushMatrix()
+        glLoadIdentity()
+        loc = self.interaction.translation
+        glTranslated(loc[0], loc[1], loc[2])
+        glMultMatrixf(self.interaction.trackball.matrix)
+
+        # store the inverse of the current modelview.
+        currentModelView = numpy.array(glGetFloatv(GL_MODELVIEW_MATRIX))
+        self.modelView = numpy.transpose(currentModelView)
+        self.inverseModelView = inv(numpy.transpose(currentModelView))
+
+        # render the scene. This will call the render function for each object
+        # in the scene
+        self.scene.render()
+
+        # draw the grid
+        glDisable(GL_LIGHTING)
+        glCallList(G_OBJ_PLANE)
+        glPopMatrix()
+
+        # flush the buffers so that the scene can be drawn
+        glFlush()
+
+    def init_view(self):
+        """ initialize the projection matrix """
+        xSize, ySize = glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT)
+        aspect_ratio = float(xSize) / float(ySize)
+
+        # load the projection matrix. Always the same
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+
+        glViewport(0, 0, xSize, ySize)
+        gluPerspective(70, aspect_ratio, 0.1, 1000.0)
+        glTranslated(0, 0, -15)
